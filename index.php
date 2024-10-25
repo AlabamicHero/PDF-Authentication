36c76564915705dee49a17f550c6f5f1c936dbdb
Dinie <60502170+AlabamicHero@users.noreply.github.com>
2024-10-25 10:13:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-// Get the requested path from the URL
-$requestedPath = $_SERVER['REQUEST_URI'];
-
-// Define the base URL of your server
-$serverBaseUrl = 'http://34.56.4.34';
-
-// Build the full URL to redirect to
-$redirectUrl = $serverBaseUrl . $requestedPath;
-
-// Send the redirect header
-header("Location: $redirectUrl", true, 302);
-exit();
-?>
+Hello World
